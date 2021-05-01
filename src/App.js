a1516e846260a2f97feef4f2e0827530be36fdf1
aakashbansal837 <aakashbansal837@gmail.com>
2021-05-01 17:16:40 +0530

    + snackbar queue

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import "./App.css";
 import "./style/index.scss";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import { useSnackbar } from "notistack";
+import { useEffect } from "react";
+import { connect } from "react-redux";
+import { hideSnackbar } from "./redux/actions/snackbarAction";
 import Homepage from "./components/Homepage";
 import Team from "./components/Team";
 import Player from "./components/Player";
 import Navbar from "./components/Navbar";
 
-function App() {
+function App(props) {
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+  useEffect(() => {
+    if (props.show_snackbar) {
+      enqueueSnackbar(props.snackbar_message, {
+        variant: props.snackbar_variant,
+        autoHideDuration: 3000,
+      });
+      props.hideSnackbar();
+    }
+  }, [props.show_snackbar]);
   return (
     <div className="App">
       <Router>
         <Navbar />
-        <Route exact path="/" component={Homepage} />
+        <Route exact path="/" component={Team} />
         <Route exact path="/teams" component={Team} />
         <Route exact path="/players" component={Player} />
       </Router>
@@ -20,4 +33,10 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  show_snackbar: state.snackbar.show,
+  snackbar_message: state.snackbar.message,
+  snackbar_variant: state.snackbar.variant,
+});
+
+export default connect(mapStateToProps, { hideSnackbar })(App);
