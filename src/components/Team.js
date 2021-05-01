a1531f25e9ec4d7bcaeaa1baae8db488ada23bc3
aakashbansal837 <aakashbansal837@gmail.com>
2021-05-01 18:59:41 +0530

    + remove player

--- src/components/Team.js
+++ src/components/Team.js
@@ -159,6 +159,89 @@ const AddNewPlayer = (props) => {
   );
 };
 
+const RemovePlayer = (props) => {
+  const [name, setName] = React.useState("");
+
+  const removePlayerData = () => {
+    if (name == "") {
+      props.showSnackbar({ message: "EMPTY PLAYER NAME", variant: "error" });
+    } else {
+      let data = {
+        name: name,
+        index: props.team_index,
+      };
+      setName("");
+      props.removePlayer(data);
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <button
+        type="button"
+        className="btn btn-danger ml-2"
+        data-toggle="modal"
+        data-target={"#playerRemove" + props.team_index}
+      >
+        Remove Player
+      </button>
+
+      <div
+        className="modal fade"
+        id={"playerRemove" + props.team_index}
+        tabIndex="-1"
+        role="dialog"
+        aria-labelledby="playerRemoveLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="playerRemoveLabel">
+                Remove Players
+              </h5>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              <select
+                className="form-control"
+                placeholder="enter Player name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              >
+                <option value="" disabled hidden>
+                  none
+                </option>
+
+                {props.players.map((pl) => (
+                  <option value={pl}>{pl}</option>
+                ))}
+              </select>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                // data-dismiss="modal"
+                className="btn btn-danger"
+                onClick={() => removePlayerData()}
+              >
+                Remove
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
 const Team = (props) => {
   const [teams, setTeams] = React.useState(props.teams);
 
@@ -181,7 +264,7 @@ const Team = (props) => {
             <th scope="col">#</th>
             <th scope="col">Teams</th>
             <th scope="col">Players</th>
-            <th scope="col">add Players</th>
+            <th scope="col">add / remove Players</th>
             <th scope="col">Remove Team</th>
           </tr>
         </thead>
@@ -202,6 +285,12 @@ const Team = (props) => {
                   <AddNewPlayer
                     team_index={index}
                     addPlayer={props.addPlayer}
+                    showSnackbar={props.showSnackbar}
+                  />
+                  <RemovePlayer
+                    team_index={index}
+                    players={team.players}
+                    removePlayer={props.removePlayer}
                     showSnackbar={props.showSnackbar}
                   />
                 </td>
