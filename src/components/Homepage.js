7bccf87634a61f025e1fd183be4412dc9e6225ef
aakashbansal837 <aakashbansal837@gmail.com>
2021-05-01 17:09:42 +0530

    + some data

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,6 +5,16 @@ const Homepage = () => {
   return (
     <div className="container homepage">
       <div className="row">landing page here</div>
+      <div className="row">
+        <a href="/teams">
+          <button className="btn btn-primary">Teams</button>
+        </a>
+
+        <a href="/players">
+          <button className="btn btn-primary">Players</button>
+        </a>
+      </div>
+      <div className="row">landing page here</div>
     </div>
   );
 };
