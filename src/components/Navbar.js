193e527ec160f8cd0f9c8a3846484684c796c7bd
aakashbansal837 <aakashbansal837@gmail.com>
2021-05-01 16:32:05 +0530

    ^ left margin

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,18 +18,18 @@ const Navbar = () => {
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
+        <ul className="navbar-nav ml-auto">
           <li className="nav-item active">
             <a className="nav-link" href="/">
               Home <span className="sr-only">(current)</span>
             </a>
           </li>
-          <li className="nav-item">
+          <li className="nav-item active">
             <a className="nav-link" href="/teams">
               Teams
             </a>
           </li>
-          <li className="nav-item">
+          <li className="nav-item active">
             <a className="nav-link" href="/players">
               players
             </a>
